aaeb7f926d8343cdc49a78d9edf57d9fe20b80e0
PC80189-PC <PC80189-PC@PC80189>
2026-06-12 17:14:01 +0900

    saya 17:13 メイン画面作成中

--- src/main/java/com/example/demo/controller/UserMenuController.java
+++ src/main/java/com/example/demo/controller/UserMenuController.java
@@ -1,5 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserMenuController {
 
+	//メイン画面の表示
+	@GetMapping("/")
+	public String index(Model model) {
+		return "userMenu";
+	}
 }
