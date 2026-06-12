3cce03a45b44f338aa5f0e9828fc187b3a906819
PC80190-PC <PC80190-PC@PC80190>
2026-06-12 13:33:28 +0900

    repository join

---
+++ src/main/java/com/example/demo/repository/ReservationdetailRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Reservationdetail;
+
+public interface ReservationdetailRepository extends JpaRepository<Reservationdetail, Integer> {
+
+}
