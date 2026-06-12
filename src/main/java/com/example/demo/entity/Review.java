815ee5432629310088cbef3ba1bbf121a7d42164
PC80189-PC <PC80189-PC@PC80189>
2026-06-12 16:00:54 +0900

    saya 16:00 Review entity

--- src/main/java/com/example/demo/entity/Review.java
+++ src/main/java/com/example/demo/entity/Review.java
@@ -1,5 +1,65 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name = "reviews")
+@Component
 public class Review {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; //レビューID
+
+	@ManyToOne
+	@JoinColumn(name = "book_id")
+	private Book book; //本ID booksテーブルのid
+
+	@Column(name = "book_review")
+	private String bookReview; //レビュー内容
+
+	//コンストラクタ
+	public Review() {
+	}
+
+	public Review(Integer id, Book book, String bookReview) {
+		this.id = id;
+		this.book = book;
+		this.bookReview = bookReview;
+	}
+
+	//ゲッターセッター
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public String getBookReview() {
+		return bookReview;
+	}
+
+	public void setBookReview(String bookReview) {
+		this.bookReview = bookReview;
+	}
 
 }
