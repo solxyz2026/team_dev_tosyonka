dd5644022d122656749dbd81b4bb6d05ae01b74b
PC80201-PC <PC80201-PC@PC80201>
2026-06-15 09:35:14 +0900

    yamamoto 6/15 9:35

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -11,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	List<User> findByEmailAndPassword(String email, String password);
 
 	List<User> findByEmailAndPasswordAndRole(String email, String password, String admin);
+
+	//aaa
 }
