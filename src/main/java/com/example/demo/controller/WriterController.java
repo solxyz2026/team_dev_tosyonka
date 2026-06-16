2273212f90cfbc9cae1e9432b1deeb1247c46b8f
PC80209-PC <PC80209-PC@PC80209>
2026-06-16 11:26:27 +0900

    6/16 11:25 WriterController.java WriterEdit.html 下山

--- src/main/java/com/example/demo/controller/WriterController.java
+++ src/main/java/com/example/demo/controller/WriterController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -58,4 +59,48 @@ public class WriterController {
 		return "redirect:/WriterAdd";
 	}
 
+	//著者更新ページへ
+	@GetMapping("/admin/writers/{writer_id}")
+	public String edit(
+			@PathVariable Integer id,
+			Model model) {
+		Writer writer = writerRepository.findById(id).get();
+		model.addAttribute("user", writer);
+		return "WriterEdit";
+	}
+
+	//著者更新
+	@PostMapping("/admin/writers/{writer_id}") //←{writer_id}とwriterIdの使い分けが不安
+	public String update(
+			@PathVariable Integer id,
+			@RequestParam(defaultValue = "") String writerName,
+			@RequestParam(defaultValue = "") String writerDescription,
+			Model model) {
+
+		ArrayList<String> list = new ArrayList<>();
+		//空欄に対するエラー
+		if (writerName.equals("")) {
+			list.add("著者を入力してください");
+		}
+		if (writerDescription.equals("")) {
+			list.add("著者紹介文を入力してください");
+		}
+		model.addAttribute("list", list);
+
+		if (writerName.equals("") || writerDescription.equals("")) {
+			Writer writer = writerRepository.findById(id).get();
+			writer.setWriterName(writerName);
+			writer.setWriterDescription(writerDescription);
+
+			model.addAttribute("writer", writer);
+			return "WriterEdit";
+		}
+
+		Writer writer = writerRepository.findById(id).get();
+		writer.setWriterName(writerName);
+		writer.setWriterDescription(writerDescription);
+		writerRepository.save(writer);
+		return "";//○○（本のタイトル）の画面へ(←未入力)
+
+	}
 }
