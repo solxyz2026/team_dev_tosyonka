e94dcdae69b5f3971e7f7c84cf73e07a37ba8376
PC80190-PC <PC80190-PC@PC80190>
2026-06-17 15:48:47 +0900

    15:45 daigo 本削除修正

--- src/main/java/com/example/demo/controller/ReviewController.java
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -1,5 +1,39 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
+
+@Controller
+@RequestMapping("/user")
 public class ReviewController {
+
+	@Autowired
+	private BookRepository bookRepository;
+
+	@GetMapping("{bookId}/review")
+	public String showReviewPage(
+			@PathVariable Integer bookId,
+			Model model) {
+
+		Optional<Book> optionalBook = bookRepository.findById(bookId);
+		model.addAttribute("book", optionalBook);
+		return "ReviewPage";
+	}
+
+	@PostMapping("/{bookId}/review")
+	public String addReview() {
+		return "BookDetail";
+
+	}
 
 }
